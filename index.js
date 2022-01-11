981dc188297054588af6157866daf62fdedc540e
Alison Melendez <alisonmelendez132@gmail.com>
2022-01-11 11:10:57 -0500

    commit tree dda4e644ab92a622eb9ed6bc2d0969f328a7d751

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide() {
+    let arthimetic = 2000 / 100
+    return arthimetic;
+}
+let square = number => {
+    return (number * number)
+}
+let add = (num1, num2) => {
+    return (num1 + num2)
+}
